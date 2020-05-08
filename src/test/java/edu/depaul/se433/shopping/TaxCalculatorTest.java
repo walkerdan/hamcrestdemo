561f3f96569cad08280fd0414ac11af7cac99fda
Dan Walker <walkerdan2@gmail.com>
2020-05-08 14:40:45 -0500

    support taxes

---
+++ src/test/java/edu/depaul/se433/shopping/TaxCalculatorTest.java
@@ -0,0 +1,14 @@
+package edu.depaul.se433.shopping;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TaxCalculatorTest {
+
+  @Test
+  @DisplayName("simple single item tax")
+  void testSimpleItemTax() {
+
+  }
+
+}
